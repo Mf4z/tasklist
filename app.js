5a130a55f350b2abf80bd81dfbc38a00bdee4304
Marwan <emef4z@gmail.com>
2020-06-23 16:52:45 +0400

    Add Local persistance for tasks

--- app.js
+++ app.js
@@ -54,12 +54,31 @@ function addTask(e){
 
     // Append li to ul
     taskList.appendChild(li);
+
+    //Store in LS
+    storeTaskInLocalStorage(taskInput.value);
+
     
     //clear input 
     taskInput.value = '';
 
-
     e.preventDefault();
+}
+
+
+//Store in Ls
+function storeTaskInLocalStorage(task){
+    let tasks;
+    if(localStorage.getItem('tasks') === null){
+        tasks = [];
+    }
+    else{
+        tasks = JSON.parse(localStorage.getItem('tasks'));
+    }
+
+    tasks.push(task);
+
+    localStorage.setItem('tasks',JSON.stringify(tasks));
 }
 
 //Remove task
